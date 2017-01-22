46c598c766bc5a6bc1ef3d4a7d411eb3a7ed93ba
rebootr <rkok@outlook.com>
2017-01-22 13:06:01 +0100

    Prefill search string in input if available

--- template/search-box.tpl.php
+++ template/search-box.tpl.php
@@ -1,6 +1,10 @@
+<?php
+$prefill = isset($searchString) ? $searchString : "";
+?>
 <form method="GET" action="">
 	<h1>TPB Search</h1>
 	<p>Enter search string:
-		<input type="text" name="search_string"><input type="submit" value="Go!">
+		<input type="text" name="search_string" value="<?php echo htmlspecialchars($prefill); ?>">
+		<input type="submit" value="Go!">
 	</p>
 </form>
