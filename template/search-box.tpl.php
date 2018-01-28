cd7aefc9218ab53e7fa808672e4e2f1fe80bb52d
rebootr <rkok@outlook.com>
2018-01-29 00:33:24 +0100

    Auto-focus on input

--- template/search-box.tpl.php
+++ template/search-box.tpl.php
@@ -4,7 +4,7 @@ $prefill = isset($searchString) ? $searchString : "";
 <form method="GET" action="">
 	<h1>TPB Search</h1>
 	<p>Enter search string:
-		<input type="text" name="search_string" value="<?php echo htmlspecialchars($prefill); ?>">
+		<input autofocus onfocus="this.scrollLeft = this.scrollWidth" type="text" name="search_string" value="<?php echo htmlspecialchars($prefill); ?>">
 		<input type="submit" value="Go!">
 	</p>
 </form>
